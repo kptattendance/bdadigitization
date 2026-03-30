94ef73ba60567610b4cd9c9251e1ebee9ee10823
kptudyogamela <kptudyogamela@gmail.com>
2026-03-30 08:45:59 +0530

    fixed

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import Navbar from "@/components/Navbar";
+import Navbar from "./components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
